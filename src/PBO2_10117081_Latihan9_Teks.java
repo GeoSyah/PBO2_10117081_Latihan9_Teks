bdd69984b40edf08bcfcc81a9c5706a78deb3adb
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 14:12:32 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan9_Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan program dengan variabel tipe data teks di Java
+ */
+public class PBO2_10117081_Latihan9_Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "Berisi " + c);
+        
+    }
+    
+}
